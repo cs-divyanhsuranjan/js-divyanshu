bb4c30d076ee7d3eae1d961abe0145f03eaafd29
cs-divyanhsuranjan <divyanshuranjan678@gmail.com>
2024-09-17 05:53:03 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144454
+let accountEmail = "divyanhsu @WebGLUniformLocation.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // Not Allowed
+
+
+accountEmail = "Kishan@gmail.com"
+accountPassword = "54336"
+accountCity = "Bihar"
+
+/*
+   Prefere Not To use Var
+   Because of issues in block scope and functional
+*/
+
+console.table([accountId ,accountState,accountEmail,     accountPassword ,accountCity]);
---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict";  // Treat all Js code as newer version
+
+//alert (3+3) //WE are using Node js ; Not browser
+
+/*
+console.log(3+
+    3)  // Code readability is high
+
+console.log("divyanshu")
+*/
+
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+
+//Number => 2 to power 53
+//bigint 
+//string =""
+//boolean => true /false
+//null => standalone value
+//undefine => variable declare but data nor defined
+//symbol => Uniqueness 
+
+
+//object 
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); // object
+
+
